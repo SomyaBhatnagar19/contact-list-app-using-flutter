01ef196b4704af3dde7cdd67cae2f448d6ce70a1
somya-roboprenr <myemail>
2024-11-10 11:43:05 +0530

    Changing the views.py in contact app to models.py and making views.py.

--- contactlist/contacts/models.py
+++ contactlist/contacts/models.py
@@ -1,3 +1,13 @@
+from django.shortcuts import render
 from django.db import models
 
-# Create your models here.
+# Create your views here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length = 100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100, blank=True, null=True) #allows the django database to 
+
+    def __str__(self):
+        return self.name
+    
--- contactlist/contacts/views.py
+++ contactlist/contacts/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
-from django.db import models
-# Create your views here.
+from rest_framework import viewsets
+from .models import Contact
+from .serializers import ContactSerializer
 
-class Contact(models.Model):
-    name = models.CharField(max_length = 100)
-    phone = models.CharField(max_length=15)
-    email = models.EmailField(max_length=100, blank=True, null=True) #allows the django database to 
 
-    def __str__(self):
-        return self.name
+class ContactViewSet(viewers.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
     
