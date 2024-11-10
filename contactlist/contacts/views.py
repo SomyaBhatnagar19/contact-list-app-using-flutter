3f02f4a6e5557c5a282763e6029312f9890c893c
somya-roboprenr <myemail>
2024-11-10 10:48:25 +0530

    Making the contact view and giving it permission in installed apps in setting.py

--- contactlist/contacts/views.py
+++ contactlist/contacts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.db import models
 # Create your views here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length = 100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=100, blank=True, null=True) #allows the django database to 
+
+    def __str__(self):
+        return self.name
+    
