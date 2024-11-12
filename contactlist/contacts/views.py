634ed1c9563b92725589f9f1ee655f78b9734f3f
somya-roboprenr <myemail>
2024-11-12 16:14:50 +0530

    Making updating contact functionality.

--- contactlist/contacts/views.py
+++ contactlist/contacts/views.py
@@ -1,8 +1,22 @@
 # contacts/views.py
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Contact
 from .serializers import ContactSerializer
+from rest_framework.response import Response
 
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+
+    # for editing the contact
+    def update(self, request, pk=None):
+        try:
+            contact = Contact.objects.get(pk=pk)
+        except Contact.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = ContactSerializer(contact, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
