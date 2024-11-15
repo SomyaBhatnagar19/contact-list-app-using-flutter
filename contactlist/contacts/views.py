d4ee70c25a0550bf12206a035f63444e0d859675
somya-roboprenr <myemail>
2024-11-15 12:13:06 +0530

    Making the changes in backend for search functionality based on name, phone no.

--- contactlist/contacts/views.py
+++ contactlist/contacts/views.py
@@ -1,5 +1,7 @@
 # contacts/views.py
 from rest_framework import viewsets, status
+from rest_framework.filters import SearchFilter
+from django.db.models import Q
 from .models import Contact
 from .serializers import ContactSerializer
 from rest_framework.response import Response
@@ -7,6 +9,8 @@ from rest_framework.response import Response
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['name', 'phone'] 
 
     # for editing the contact
     def update(self, request, pk=None):
@@ -20,3 +24,14 @@ class ContactViewSet(viewsets.ModelViewSet):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    # searching by filtering mechanism
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.query_params.get('search', None)
+        if search_query:
+            # Using Q to combine multiple conditions for name and phone
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) | Q(phone__icontains=search_query)
+            )
+        return queryset
